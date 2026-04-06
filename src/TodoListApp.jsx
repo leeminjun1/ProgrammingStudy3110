fb0f41bd635b8c2956135fed3fcb4bae27d879b5
leeminjun1 <lmjdooda@gmail.com>
2026-04-06 14:36:10 +0900

    TodoListApp 만들기

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,3 +1,28 @@
 export default function TodoListApp() {
-    
+    return (
+        <>
+            <h1>ToDoList</h1>
+            <div>
+                <form action="">
+                    <input type="text" placeholder="할 일을 입력하세요"/>
+                    <button>Add</button>
+                </form>
+                <ul>
+                    <li>
+                        <input type="checkbox" name="" id="" />
+                        <label htmlFor="chk-1">놀기</label>
+                        <button>🥄</button>
+                        <button>🤣</button>
+                    </li>
+                     <li>
+                        <input type="checkbox" name="" id="" />
+                        <label htmlFor="chk-2">놀기</label>
+                        <button>🥄</button>
+                        <button>🤣</button>
+                    </li>
+                </ul>
+            </div>
+            
+        </>
+    )
 }
