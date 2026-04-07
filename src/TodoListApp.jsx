40045b032db4e0e328aba63f86898d6fdd9d02f7
leeminjun1 <lmjdooda@gmail.com>
2026-04-07 15:26:20 +0900

    TodoListApp HTML className, todolist css

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,19 +1,21 @@
+import './todolist.css'
+
 export default function TodoListApp() {
     return (
         <>
-            <div>
-                <h1>ToDoList</h1>
+            <div className='todo'>
+                <h1 className='todo__title'>ToDoList</h1>
                 <div>
-                    <form action="">
-                        <input type="text" placeholder="할 일을 입력하세요"/>
-                        <button>Add</button>
+                    <form action="" className='todo__form'>
+                        <input type="text" placeholder="할 일을 입력하세요" className='todo__input'/>
+                        <button type="submit" className='todo__button todo__button--add'>Add</button>
                     </form>
-                    <ul>
-                        <li>
-                            <input type="checkbox" name="" id="" />
-                            <label htmlFor="chk-1">놀기</label>
-                            <button>🥄</button>
-                            <button>🤣</button>
+                    <ul className='todo__list'>
+                        <li className='todo__item todo__item--complete'>
+                            <input type="checkbox" name="" id="" className='todo__check'/>
+                            <label htmlFor="chk-1" className='todo__label'>놀기</label>
+                            <button className='todo__button todo__button--edit'>🥄</button>
+                            <button className='todo__button todo__button--delete'>🤣</button>
                         </li>
                         <li>
                             <input type="checkbox" name="" id="" />
